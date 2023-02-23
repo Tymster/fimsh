2af36dcb76a86f631640974e66611cc137b8cfee
Tymek Lewandowski <tymek@tymeks-air.lan>
2023-02-23 17:43:09 +0000

    quantum fish deleted

--- src/fish.rs
+++ src/fish.rs
@@ -1,10 +1,49 @@
+use async_std::process::Command;
+use mongodb::bson::doc;
 use mongodb::bson::Document;
+use rand;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+use uuid;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Fish {
     id: u32,
     rating: i32,
+    name: String,
 }
 impl Fish {
-    pub fn new() {}
+    pub async fn grr(collection: &mongodb::Collection<Document>) -> std::io::Result<()> {
+        let names: Vec<String> = std::fs::read_to_string("names.txt")
+            .unwrap()
+            .split("\n")
+            .map(|f| f.to_string())
+            .collect();
+        collection.delete_many(doc! {}, None).await.unwrap();
+        Command::new("mkdir").arg("temp").spawn().unwrap();
+        for (i, img) in std::fs::read_dir("./images").unwrap().enumerate() {
+            std::fs::rename(
+                img.unwrap().path().display().to_string(),
+                format!("./temp/{i}.jpg"),
+            )?;
+            collection
+                .insert_one(
+                    doc! {
+                        "id" : i as u32,
+                        "rating" : 100,
+                        "name" : &names[rand::thread_rng().gen_range(0..names.len())],
+                    },
+                    None,
+                )
+                .await
+                .unwrap();
+        }
+        for n in std::fs::read_dir("./temp").unwrap() {
+            std::fs::copy(
+                &n.as_ref().unwrap().path(),
+                format!("./images/{}", &n.unwrap().file_name().to_str().unwrap()),
+            )?;
+        }
+        Command::new("rm").arg("-r").arg("temp").spawn().unwrap();
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod fish;
 use dotenv::dotenv;
+use fish::Fish;
 use mongodb::bson::{from_document, Bson, Document};
 use mongodb::{bson::doc, Client};
 use tide::prelude::*;
@@ -20,10 +21,10 @@ impl Fishes {
             collection: client.database("fish").collection::<Document>("fish"),
         })
     }
-    pub async fn handle_update(req: Request<Fishes>) -> tide::Result {
+    pub async fn fish_update(req: Request<Fishes>) -> tide::Result {
         let fish = find_fish(
             &req.state().collection,
-            req.param("id").unwrap().parse::<u32>().unwrap(),
+            req.param("id").unwrap().parse::<String>().unwrap(),
         )
         .await
         .unwrap();
@@ -33,30 +34,43 @@ impl Fishes {
 }
 #[derive(Serialize, Deserialize, Debug)]
 struct FishUpdate {
-    id: u32,
+    id: String,
     value: i32,
 }
 #[tokio::main]
 async fn main() -> tide::Result<()> {
-    let fishes = Fishes::connect().await?;
-    dotenv().ok();
-
-    let mut app = tide::with_state(Fishes::connect().await.unwrap());
-    println!("New app made");
-    app.at("/update/:id").post(Fishes::handle_update);
-    app.listen("127.0.0.1:8080").await.unwrap();
     // let url = format!(
     //     "mongodb+srv://user:{}@cluster.2qxtmxu.mongodb.net/?retryWrites=true&w=majority",
     //     std::env::var("PASSWORD").unwrap()
     // );
+    let fishes = Fishes::connect().await?;
+    Fish::grr(&fishes.collection).await?;
+    dotenv().ok();
+    let mut app = tide::with_state(Fishes::connect().await.unwrap());
+    println!("New app made");
+    app.at("/update/:id").post(Fishes::fish_update);
+    app.at("/cdn/:id").get(handle_image);
+    app.listen("127.0.0.1:8080").await.unwrap();
     Ok(())
 }
-async fn handle_update(req: Request<Fishes>) -> tide::Result {
-    Ok("jerome".into())
+async fn handle_image(req: Request<Fishes>) -> tide::Result {
+    let id = req.param("id").unwrap();
+    match std::path::Path::new(&format!("./images/{id}.png")).exists() {
+        false => Ok("Not found".into()),
+        _ => {
+            let mut res = tide::Response::new(tide::StatusCode::Ok);
+            res.set_body(
+                tide::Body::from_file(format!("./images/{id}.jpg"))
+                    .await
+                    .unwrap(),
+            );
+            Ok(res.into())
+        }
+    }
 }
 async fn find_fish(
     collection: &mongodb::Collection<Document>,
-    id: u32,
+    id: String,
 ) -> Result<fish::Fish, mongodb::bson::de::Error> {
     Ok(from_document(
         collection
